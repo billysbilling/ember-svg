945a355ef5807c22143df9830673bd551a5b7814
Sebastian Seilund <sebastian@seilund.com>
2013-10-23 19:24:40 -0700

    added initial files

---
+++ Gruntfile.js
@@ -0,0 +1,9 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        billy_builder: {
+            title: 'ember-svg'
+        }
+    });
+
+    grunt.loadNpmTasks('billy-builder');
+};
